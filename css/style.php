2bbb8fa912a96f5014663a4fc5dd073ca3ca76d3
Mathexcel <32784179+Mathexcel@users.noreply.github.com>
2018-08-25 11:46:55 -0300

    Add files via upload

--- css/style.php
+++ css/style.php
@@ -43,4 +43,65 @@
 				margin-left: 0px;
 				margin-bottom: 0px;
 			}
+
+			#font_titulo_menu_lateral {
+				font-family: Lucida Calligraphy;
+				font-size: 25px;
+				color: #fff;
+				text-align: center;
+			}
+
+			#font_subtitulo_menu_lateral {
+				font-family: Lucida Calligraphy;
+				font-size: 15px;
+				color: #fff;
+				text-align: center;
+				line-height: 100px;
+			}
+
+			#design_modal {
+				margin-left: 0px;
+				margin-top: 0px;
+				margin-bottom: 0px;
+				background-image: linear-gradient(to bottom, #f97000, black);
+			}
+
+			#font_titulo_modal {
+				font-family: Lucida Calligraphy;
+				font-size: 25px;
+				color: #fff;
+				text-align: center;
+			}
+
+			#font_modal {
+				font-family: Lucida Calligraphy;
+				font-size: 15px;
+				color: #fff;
+				text-align: center;
+				line-height: 60px;
+			}
+
+			#back_produtos {
+				background-image: url("images/Produtos.png");
+				width: 1026px;
+				height: 576px;
+				margin-top: -576px;
+				margin-left: 340px;
+				margin-bottom: 0px;
+				font-family: Lucida Calligraphy;
+				font-size: 15px;
+				color: black;
+				text-align: center;
+				line-height: 525px;
+			}
+
+			#design_quadrado {
+				width: 225px;
+				height: 225px;
+				background-image: linear-gradient(to bottom, black, #f97000);				
+				font-family: Lucida Calligraphy;
+				font-size: 12px;
+				color: #fff;
+				line-height: 18px;				
+			}
 		</style>
